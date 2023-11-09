7e37b732d5e63266f28608fa081b7e95899e3320
Kianmehr Kimiavi <85435062+KianmehrKM@users.noreply.github.com>
2023-11-09 04:12:24 -0800

    Create Program5 23-11-08 Homework.cs

---
+++ C# & Database/Program5 23-11-08 Homework.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, i, ii;
+            float fact = 1;
+            float sum = 0;
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= number; i++)
+            {
+                for (ii = 1; ii <= i; ii++)
+                {
+                    fact = fact * ii;
+                }
+                sum = sum + fact;
+            }
+
+            Console.WriteLine("Your result for FACTORIAL is: " + fact);
+            Console.WriteLine("Your result for SUM is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
