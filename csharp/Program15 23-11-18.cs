1bf88c92c0378b784d7789ef5f2d2455fbb0e1a1
Kianmehr Kimiavi <85435062+KianmehrKM@users.noreply.github.com>
2023-11-20 16:27:34 +0330

    Create Program15 23-11-18.cs

---
+++ csharp/Program15 23-11-18.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 5)
+        {
+            int j = 5;
+            while (j >= i)
+            {
+                Console.Write(j);
+                j--;
+            }
+            Console.WriteLine();
+            i++;
+        }
+    }
+}
+
+/* This is the code for the following output:
+ * 54321
+ * 5432
+ * 543
+ * 54
+ * 5
+ */
