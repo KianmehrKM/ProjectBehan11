540fdcd0313bcf84fda38976c0e4c57eeea948b0
Kianmehr Kimiavi <85435062+KianmehrKM@users.noreply.github.com>
2023-11-20 16:28:53 +0330

    Create Program16 23-11-18.cs

---
+++ csharp/Program16 23-11-18.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+
+        Console.Write("Enter a number: ");
+        number = int.Parse(Console.ReadLine());
+
+        int i = 1;
+        while (i <= number)
+        {
+            int j = 1;
+            while (j <= number - i + 1)
+            {
+                Console.Write(j);
+                j++;
+            }
+            Console.WriteLine();
+            i++;
+        }
+    }
+}
+
+/* This is the code for the following output:
+ * Number: 9 (as an exmaple)
+ * Output:
+ * 123456789
+ * 12345678
+ * 1234567
+ * 123456
+ * 12345
+ * 1234
+ * 123
+ * 12
+ * 1
+ */
